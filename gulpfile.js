0c71d2016121a2f2e8157f3a988fb58d03af140a
Myk Melez <myk@mozilla.org>
2015-09-09 07:34:01 -0700

    add basic gulpfile; ignore dist/ dir

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 Mozilla
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var packageJson = require('./package.json');
+var path = require('path');
+var swPrecache = require('sw-precache');
+
+gulp.task('default', ['copy-app-to-dist', 'generate-service-worker']);
+
+gulp.task('copy-app-to-dist', function(callback) {
+  return gulp.src('app/**').pipe(gulp.dest('dist'));
+});
+
+gulp.task('generate-service-worker', ['copy-app-to-dist'], function(callback) {
+  swPrecache.write(path.join('dist', 'ophliner-worker.js'), {
+    cacheId: packageJson.name,
+    staticFileGlobs: [
+      'dist/**/*.css',
+      'dist/**/*.html',
+      'dist/**/*.js',
+    ],
+    stripPrefix: 'dist/',
+  }, callback);
+});
