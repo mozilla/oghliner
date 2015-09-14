f7faec42a5ba43e96dc7d0d56c75503bb101235f
Myk Melez <myk@mozilla.org>
2015-09-14 16:09:02 -0700

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -24,7 +24,7 @@
 
 
 
-var PrecacheConfig = [["index.html","511d1a004cfbad2917b8f0eca4ad46aa"],["scripts/offline-manager.js","28bd78ca4fcf8ae2b562c51a1cd2f5d3"]];
+var PrecacheConfig = [["index.html","6dcf78300d9d3d47fad031cc15aea38f"],["scripts/offline-manager.js","28bd78ca4fcf8ae2b562c51a1cd2f5d3"]];
 var CacheNamePrefix = 'sw-precache-v1-oghliner-' + (self.registration ? self.registration.scope : '') + '-';
 
 
