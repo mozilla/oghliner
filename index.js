fc7127397bb4374640dceab2058fc0c6f274567b
Myk Melez <myk@mozilla.org>
2015-09-18 14:44:31 -0700

    get a Travis token via a temp GitHub token

--- index.js
+++ index.js
@@ -23,10 +23,35 @@ var getGitHubToken = promisify(require('get-github-token'));
 var ghPages = require('gh-pages');
 var gitconfiglocal = require('gitconfiglocal');
 var path = require('path');
+var promptly = require('promptly');
 var swPrecache = require('sw-precache');
 var readYaml = require('read-yaml');
 var travisEncrypt = promisify(require('travis-encrypt'));
 var writeYaml = require('write-yaml');
+
+var Travis = require('travis-ci');
+var travis = new Travis({ version: '2.0.0' });
+
+var GitHub = require('github');
+var github = new GitHub({
+  version: "3.0.0",
+  debug: true, // XXX Disable once things are working fine.
+  protocol: "https", // XXX Determine if this is already the default.
+  headers: {
+    "user-agent": "Oghliner",
+  },
+});
+
+// promisify should be able to wrap the entire promptly API
+// via promisify(promptly), but that didn't seem to work, so here
+// we wrap the individual methods we use.
+promptly.prompt = promisify(promptly.prompt);
+promptly.password = promisify(promptly.password);
+
+// Wrap some more individual methods for complex APIs that I'm scared to wrap
+// in toto.
+github.authorization.delete = promisify(github.authorization.delete);
+travis.hooks.get = promisify(travis.hooks.get);
 
 /**
  * Get the slug (GitHub username/repo combination) for the 'origin' remote
@@ -57,6 +82,25 @@ function getSlug(callback) {
 }
 getSlug = promisify(getSlug);
 
+// XXX Could we promisify github.authorization.create with promisify?
+function createGitHubAuthorization(scopes, note, noteUrl, headers) {
+  return new Promise(function(resolve, reject) {
+    github.authorization.create({
+      scopes: scopes,
+      note: note,
+      note_url: noteUrl,
+      headers: headers,
+    }, function(err, res) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(res);
+      }
+    });
+  });
+}
+
 function configure(callback) {
   process.stdout.write(
     '\n' +
@@ -65,13 +109,15 @@ function configure(callback) {
     '\n'
   );
 
-  // Save the slug in the closure so we can use it across the promise chain
-  // without having to pass it down the chain.
-  var slug;
+  // Save some values in the closure so we can use them across the promise chain
+  // without having to pass them down the chain.
+  var slug, username, password, otpCode, tempTokenId;
 
   getSlug()
-  .then(function(aSlug) {
-    slug = aSlug;
+
+  .then(function(result) {
+    slug = result;
+
     process.stdout.write(
       'The "origin" remote of your repository is "' + slug + '".\n' +
       '\n' +
@@ -87,21 +133,101 @@ function configure(callback) {
       'for the token I create, see https://github.com/settings/tokens.\n' +
       '\n'
     );
+  })
 
+  .then(function() {
+    return promptly.prompt('Username: ', { default: slug.split('/')[0] });
+  })
+
+  .then(function(result) {
+    username = result;
+
+    return promptly.password('Password: ');
+  })
+
+  .then(function(result) {
+    password = result;
+
+    github.authenticate({
+      type: 'basic',
+      username: username,
+      password: password
+    });
+  })
+
+  .then(function() {
+    var scopes = ['read:org', 'user:email', 'repo_deployment', 'repo:status', 'write:repo_hook'];
+    var note = 'temporary Oghliner token to get Travis token for ' + slug;
+    var noteUrl = 'https://github.com/mozilla/oghliner';
+
+    // Create a temporary GitHub token to get a Travis token that we can use
+    // to activate the repository in Travis.  We only need this GitHub token
+    // to get the Travis token, so we delete it afterward.  (We do also need
+    // a GitHub token that enables Travis to deploy a build to GitHub Pages,
+    // but we get a separate token for that purpose afterward).
+
+    // NB: The GitHub authorization API always requires basic authentication,
+    // so it isn't possible to request a token that gives us access to it.
+    // Otherwise we'd do that first and then use it to do everything else.
+
+    return createGitHubAuthorization(scopes, note, noteUrl, {})
+    .catch(function(err) {
+      var message = JSON.parse(err.message).message;
+      // XXX Also handle the case where the credentials were incorrect.
+      if (message === 'Must specify two-factor authentication OTP code.') {
+        // XXX Display explanatory text so the user knows why we're prompting
+        // for their OTP code.
+        return promptly.prompt('Auth Code: ')
+        .then(function(res) {
+          otpCode = res;
+          return createGitHubAuthorization(scopes, note, noteUrl, { 'X-GitHub-OTP': otpCode });
+        });
+      }
+    });
+  })
+
+  .then(function(res) {
+    // Record the ID of the temporary GitHub token so we can delete it
+    // after we finish getting the Travis token.
+    tempTokenId = res.id;
+
+    return new Promise(function(resolve, reject) {
+      travis.authenticate({
+        github_token: res.token,
+      }, function (err, res) {
+        err ? reject(err) : resolve(res);
+      });
+    });
+
+  })
+
+  .then(function(res) {
+    console.log("Travis access token: " + res.access_token);
+
+    // We don't need to save the access token, because the Travis module
+    // caches it in the Travis instance.
+
+    // Now that we have the Travis token, delete the temporary GitHub token.
+    // XXX Handle the case where the OTP code has already expired.
+    return github.authorization.delete({
+      id: tempTokenId,
+      headers: otpCode ? { 'X-GitHub-OTP': otpCode } : {},
+    });
+  })
+
+  .then(function() {
+    // XXX Ensure that the repository is known by and active in Travis.
+    // XXX If necessary, sync Travis with GitHub and/or activate the repository.
+  })
+
+  .then(function() {
+    // XXX Get this token at the same time we get the temporary token,
+    // so we can use a single 2FA code for both requests.
     var note = 'Oghliner token for ' + slug;
     var url = 'https://github.com/mozilla/oghliner';
     return getGitHubToken(['public_repo'], note, url);
-  },
-  function(err) {
-    process.stdout.write(
-      '\n' +
-      'I wasn\'t able to determine your GitHub username and repo.  Are you running\n' +
-      'this command in a Git repository whose "origin" remote is on GitHub?  The error\n' +
-      'I received is: ' + err + '\n' +
-      '\n'
-    );
-    callback(err);
   })
+
   .then(function(token) {
     process.stdout.write(
       '\n' +
@@ -113,6 +239,7 @@ function configure(callback) {
 
     return travisEncrypt(slug, 'GH_TOKEN=' + token, undefined, undefined);
   })
+
   .then(function(blob) {
     process.stdout.write(
       'I encrypted the token. Next I\'ll write it to the Travis configuration…\n' +
@@ -132,6 +259,7 @@ function configure(callback) {
     travisYml.env.global.push({ secure: blob });
     writeYaml.sync('.travis.yml', travisYml);
   })
+
   .then(function() {
     process.stdout.write(
       'I wrote the encrypted token to the Travis configuration.  You\'re ready\n' +
@@ -146,12 +274,15 @@ function configure(callback) {
       '\n'
     );
   })
+
   .catch(function(err) {
     callback(err);
   })
+
   .done(function() {
     callback();
   });
+
 }
 
 function offline(config, callback) {
