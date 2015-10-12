a050d69af63bcc913b439297f45ff7d855893ff9
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-12 14:02:29 +0200

    Move tests under test/ directory

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('serve', function () {
 });
 
 gulp.task('test', function () {
-  return gulp.src('test.js', {read: false})
+  return gulp.src('test/test*.js', {read: false})
     // gulp-mocha needs filepaths so you can't have any plugins before it
     .pipe(mocha());
 })
