90a296ace8ea6c9807577f2c53e24ac359b5ee7a
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-13 17:28:50 +0200

    Test deploying when a gh-pages branch already exists

--- test/testDeploy.js
+++ test/testDeploy.js
@@ -23,13 +23,65 @@ describe('Deploy', function() {
                       .addRemote('origin', path.join(process.cwd(), 'tmp'), function() {
         process.chdir('tmp');
 
-        return deploy({}).then(function() {
+        return deploy({
+          cloneDir: '.gh-pages-cache',
+        }).then(function() {
           process.chdir('..');
 
           return simpleGit.checkout('gh-pages').log(function(err, log) {
-            assert.equal(log.total, 1, '1 commit');
-            assert.equal(fs.readFileSync('tmp/file', 'utf8'), 'data');
-            finish();
+            try {
+              assert.equal(log.total, 1, '1 commit');
+              assert.equal(fs.readFileSync('tmp/file', 'utf8'), 'data');
+              finish();
+            } catch (e) {
+              finish(e);
+            }
+          });
+        }, function() {
+          process.chdir('..');
+          assert(false, 'Deploy\'s promise should be resolved');
+        }).catch(finish);
+      });
+    });
+  });
+
+  it('should update the gh-pages branch in the origin repo and publish files to it', function(done) {
+    fs.mkdirSync('tmp');
+
+    function finish(err) {
+      fse.removeSync('tmp');
+      done(err);
+    }
+
+    var simpleGit = require('simple-git')('tmp');
+
+    simpleGit.init(function() {
+      fs.writeFileSync('tmp/file1', 'data1');
+      fs.writeFileSync('tmp/file2', 'data2')
+
+      return simpleGit.add('file1')
+                      .commit('Initial commit')
+                      .addRemote('origin', path.join(process.cwd(), 'tmp'))
+                      .checkoutLocalBranch('gh-pages')
+                      .add('file2')
+                      .commit('Commit in gh-pages')
+                      .checkout('master', function() {
+        process.chdir('tmp');
+
+        return deploy({
+          cloneDir: '.gh-pages-cache',
+        }).then(function() {
+          process.chdir('..');
+
+          return simpleGit.checkout('gh-pages').log(function(err, log) {
+            try {
+              assert.equal(log.total, 3, '3 commits');
+              assert.equal(fs.readFileSync('tmp/file1', 'utf8'), 'data1');
+              assert(!fs.existsSync('tmp/file2'), 'Old files are removed when deploying');
+              finish();
+            } catch (e) {
+              finish(e);
+            }
           });
         }, function() {
           process.chdir('..');
