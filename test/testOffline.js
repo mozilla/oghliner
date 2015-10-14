be7fd854cad2532d883f736085762b2dc9fe10d1
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-15 01:40:01 +0200

    Test different options for cache ID

--- test/testOffline.js
+++ test/testOffline.js
@@ -2,9 +2,21 @@ var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 var temp = require('temp').track();
-var offline = require('../lib/offline');
+var ghslug = require('github-slug');
+var rewire = require('rewire');
+var offline = rewire('../lib/offline');
 
 describe('Offline', function() {
+  var oldWd;
+  beforeEach(function() {
+    oldWd = process.cwd();
+  });
+
+  afterEach(function() {
+    process.chdir(oldWd);
+    offline.__set__('ghslug', ghslug);
+  });
+
   it('should create offline-worker.js in the destination directory', function() {
     var dir = temp.mkdirSync('tmp');
 
@@ -39,6 +51,58 @@ describe('Offline', function() {
     }).then(function() {
       var content = fs.readFileSync(path.join(dir, 'offline-worker.js'), 'utf8');
       assert.notEqual(content.indexOf('importScripts("a-script.js");'), -1);
+    });
+  });
+
+  it('should use the GitHub slug as the cache ID if it is available', function() {
+    var rootDir = temp.mkdirSync('tmp');
+    var dir = path.join(rootDir, 'dist');
+    fs.mkdirSync(dir);
+
+    offline.__set__('ghslug', function(path, callback) {
+      callback(null, 'mozilla/oghliner');
+    });
+
+    process.chdir(rootDir);
+
+    return offline({
+      rootDir: dir,
+    }).then(function() {
+      var content = fs.readFileSync(path.join(dir, 'offline-worker.js'), 'utf8');
+      assert.notEqual(content.indexOf('mozilla/oghliner'), -1);
+    });
+  });
+
+  it('should use the app name from package.json as the cache ID if the GitHub slug is not available', function() {
+    var rootDir = temp.mkdirSync('tmp');
+    var dir = path.join(rootDir, 'dist');
+    fs.mkdirSync(dir);
+
+    fs.writeFileSync(path.join(rootDir, 'package.json'), JSON.stringify({
+      name: 'test_name',
+    }));
+
+    process.chdir(rootDir);
+
+    return offline({
+      rootDir: dir,
+    }).then(function() {
+      var content = fs.readFileSync(path.join(dir, 'offline-worker.js'), 'utf8');
+      assert.notEqual(content.indexOf('test_name'), -1);
+    });
+  });
+
+  it('should not fail if both the GitHub slug and package.json are not available', function() {
+    var rootDir = temp.mkdirSync('tmp');
+    var dir = path.join(rootDir, 'dist');
+    fs.mkdirSync(dir);
+
+    process.chdir(rootDir);
+
+    return offline({
+      rootDir: dir,
+    }).then(function() {
+      assert.doesNotThrow(fs.accessSync.bind(fs, path.join(dir, 'offline-worker.js')));
     });
   });
 });
