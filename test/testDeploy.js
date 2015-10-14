b226e64590c4489ec6a743e308e7683b6317f6a4
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-14 14:27:34 +0200

    Check that accessSync throws an exception instead of !fs.existsSync

--- test/testDeploy.js
+++ test/testDeploy.js
@@ -77,7 +77,7 @@ describe('Deploy', function() {
             try {
               assert.equal(log.total, 3, '3 commits');
               assert.equal(fs.readFileSync('tmp/file1', 'utf8'), 'data1');
-              assert(!fs.existsSync('tmp/file2'), 'Old files are removed when deploying');
+              assert.throws(fs.accessSync.bind(fs, 'tmp/file2'), 'Old files are removed when deploying');
               finish();
             } catch (e) {
               finish(e);
