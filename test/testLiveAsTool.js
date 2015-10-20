be5b6565065c73e2b53da14a54a8197f1c18c5c0
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-10-20 03:15:21 +0200

    Add live test for Oghliner used as a tool with CLI commands

---
+++ test/testLiveAsTool.js
@@ -0,0 +1,137 @@
+var promisify = require('promisify-node');
+
+var assert = require('assert');
+var path = require('path');
+var fse = require('fs-extra');
+var childProcess = require('child_process');
+var promptly = require('promptly');
+var temp = require('temp').track();
+
+var GitHub = require('github');
+var github = new GitHub({
+  version: '3.0.0',
+  protocol: 'https',
+  headers: {
+    'user-agent': 'Oghliner',
+  },
+});
+
+promptly.prompt = promisify(promptly.prompt);
+promptly.password = promisify(promptly.password);
+
+process.chdir(temp.mkdirSync('oghliner'));
+
+var username = process.env.USER, password = process.env.PASS;
+
+function createRepo() {
+  return new Promise(function(resolve, reject) {
+    github.repos.create({
+      name: 'test_oghliner_live',
+      auto_init: true,
+    }, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function deleteRepo() {
+  return new Promise(function(resolve, reject) {
+    github.repos.delete({
+      user: username,
+      repo: 'test_oghliner_live',
+    }, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function getBranch() {
+  return new Promise(function(resolve, reject) {
+    github.repos.getBranch({
+      user: username,
+      repo: 'test_oghliner_live',
+      branch: 'gh-pages',
+    }, function(err, res) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+function spawn(command, args) {
+  return new Promise(function(resolve, reject) {
+    var child = childProcess.spawn(command, args);
+
+    child.stdout.on('data', function(chunk) {
+      process.stdout.write(chunk);
+    });
+
+    child.stderr.on('data', function(chunk) {
+      process.stderr.write(chunk);
+    });
+
+    child.on('exit', function(code, signal) {
+      if (code === 0) {
+        resolve(code);
+      } else {
+        reject(code);
+      }
+    });
+
+    child.on('error', function(err) {
+      reject(err);
+    });
+  });
+}
+
+github.authenticate({
+  type: 'basic',
+  username: username,
+  password: password,
+});
+
+process.env.GH_TOKEN = username + ':' + password;
+
+describe('CLI interface, oghliner as a tool', function() {
+  this.timeout(60000);
+
+  beforeEach(function() {
+    return deleteRepo();
+  });
+
+  it('should work', function() {
+    return createRepo()
+    .then(() => spawn('git', ['clone', 'https://' + username + ':' + password + '@github.com/' + username + '/test_oghliner_live']))
+    .then(() => process.chdir('test_oghliner_live'))
+    .then(() => spawn(path.join(path.dirname(__dirname), 'cli.js'), ['offline', '.']))
+    .then(() => spawn(path.join(path.dirname(__dirname), 'cli.js'), ['integrate', '.']))
+    .then(() => spawn(path.join(path.dirname(__dirname), 'cli.js'), ['deploy', '.']))
+    .then(getBranch)
+    .then(function() {
+      fse.readdirSync('.').forEach(function(file) {
+        if (file === '.git') {
+          return;
+        }
+
+        fse.removeSync(file);
+      });
+    })
+    .then(() => spawn('git', ['checkout', '-b', 'gh-pages']))
+    .then(() => spawn('git', ['pull', 'origin', 'gh-pages']))
+    .then(function() {
+      assert.doesNotThrow(() => fse.statSync('offline-manager.js'));
+      assert.doesNotThrow(() => fse.statSync('offline-worker.js'));
+    });
+  });
+});
