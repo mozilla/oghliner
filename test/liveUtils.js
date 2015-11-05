7103f8a84d8310109cd210988796ffb36430660a
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-05 10:48:16 +0000

    Move live tests common functionality in a separate module

---
+++ test/liveUtils.js
@@ -0,0 +1,208 @@
+var childProcess = require('child_process');
+var readlineSync = require('readline-sync');
+
+var GitHub = require('github');
+var github = new GitHub({
+  version: '3.0.0',
+  protocol: 'https',
+  headers: {
+    'user-agent': 'Oghliner',
+  },
+});
+
+var liveUtils = {
+  githubToken: null,
+  createAuthorization: createAuthorization,
+  createRepo: createRepo,
+  deleteRepo: deleteRepo,
+  getBranch: getBranch,
+  spawn: spawn,
+};
+
+var githubTokenId;
+var useOTP = false;
+
+function createRepo() {
+  return new Promise(function(resolve, reject) {
+    github.repos.create({
+      name: 'test_oghliner_live',
+      auto_init: true,
+    }, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function deleteRepo(username) {
+  return new Promise(function(resolve, reject) {
+    github.repos.delete({
+      user: username,
+      repo: 'test_oghliner_live',
+    }, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function getBranch(username) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      github.repos.getBranch({
+        user: username,
+        repo: 'test_oghliner_live',
+        branch: 'gh-pages',
+      }, function(err, res) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    }, 3000);
+  });
+}
+
+function getTokenId(page) {
+  page = page || 1;
+
+  return new Promise(function(resolve, reject) {
+    github.authorization.getAll({
+      page: page,
+      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
+    }, function(err, res) {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].note === 'test' && res[i].note_url === 'http://www.test.org') {
+          resolve(res[i].id);
+          return;
+        }
+      }
+
+      resolve(getTokenId(++page));
+    });
+  });
+}
+
+function deleteAuthorization(tokenId) {
+  return new Promise(function(resolve, reject) {
+    github.authorization.delete({
+      id: tokenId,
+      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
+    }, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function createAuthorization(username, password) {
+  return new Promise(function(resolve, reject) {
+    github.authenticate({
+      type: 'basic',
+      username: username,
+      password: password,
+    });
+
+    github.authorization.create({
+      scopes: ['repo', 'public_repo', 'delete_repo'],
+      note: 'test',
+      note_url: 'http://www.test.org',
+      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
+    }, function(err, res) {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      liveUtils.githubToken = res.token;
+      githubTokenId = res.id;
+
+      github.authenticate({
+        type: 'oauth',
+        token: liveUtils.githubToken,
+      });
+
+      resolve(res);
+    });
+  }).catch(function(err) {
+    var error = JSON.parse(err.message);
+
+    if (error.message === 'Must specify two-factor authentication OTP code.') {
+      useOTP = true;
+      return createAuthorization(uesrname, password);
+    }
+
+    if (error.message === 'Validation Failed' && error.errors[0].code === 'already_exists') {
+      return getTokenId().then(deleteAuthorization).then(createAuthorization.bind(null, username, password));
+    }
+
+    throw err;
+  });
+}
+
+function spawn(command, args, expected) {
+  return new Promise(function(resolve, reject) {
+    var child = childProcess.spawn(command, args);
+
+    child.stdout.on('data', function(chunk) {
+      process.stdout.write(chunk);
+    });
+
+    child.stderr.on('data', function(chunk) {
+      process.stderr.write(chunk);
+    });
+
+    if (expected) {
+      var output = '';
+      var nextExpected = expected.shift();
+
+      child.stdout.on('data', function(chunk) {
+        output += chunk.toString();
+
+        if (nextExpected && output.indexOf(nextExpected.q) != -1) {
+          if (typeof nextExpected.r === 'function') {
+            child.stdin.write(nextExpected.r() + '\n');
+          } else {
+            child.stdin.write(nextExpected.r + '\n');
+          }
+
+          if (expected.length > 0) {
+            nextExpected = expected.shift();
+            output = '';
+          } else {
+            nextExpected = null;
+          }
+        }
+      });
+    }
+
+    child.on('exit', function(code, signal) {
+      if (code === 0) {
+        resolve(code);
+      } else {
+        reject(code);
+      }
+    });
+
+    child.on('error', function(err) {
+      reject(err);
+    });
+  });
+}
+
+module.exports = liveUtils;
--- test/testLiveAsTool.js
+++ test/testLiveAsTool.js
@@ -2,204 +2,16 @@ var assert = require('assert');
 var expect = require('chai').expect;
 var path = require('path');
 var fse = require('fs-extra');
-var childProcess = require('child_process');
 var readYaml = require('read-yaml');
 var temp = require('temp').track();
 var readlineSync = require('readline-sync');
-
-var GitHub = require('github');
-var github = new GitHub({
-  version: '3.0.0',
-  protocol: 'https',
-  headers: {
-    'user-agent': 'Oghliner',
-  },
-});
+var liveUtils = require('./liveUtils');
 
 var username = process.env.USER, password = process.env.PASS;
-var githubToken, githubTokenId;
-var useOTP = false;
 
 // Skip these tests if the USER or PASS environment variables aren't set.
 if (!username || !password) {
   return;
-}
-
-function createRepo() {
-  return new Promise(function(resolve, reject) {
-    github.repos.create({
-      name: 'test_oghliner_live',
-      auto_init: true,
-    }, function(err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function deleteRepo() {
-  return new Promise(function(resolve, reject) {
-    github.repos.delete({
-      user: username,
-      repo: 'test_oghliner_live',
-    }, function(err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function getBranch() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      github.repos.getBranch({
-        user: username,
-        repo: 'test_oghliner_live',
-        branch: 'gh-pages',
-      }, function(err, res) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(res);
-        }
-      });
-    }, 3000);
-  });
-}
-
-function spawn(command, args, expected) {
-  return new Promise(function(resolve, reject) {
-    var child = childProcess.spawn(command, args);
-
-    child.stdout.on('data', function(chunk) {
-      process.stdout.write(chunk);
-    });
-
-    child.stderr.on('data', function(chunk) {
-      process.stderr.write(chunk);
-    });
-
-    if (expected) {
-      var output = '';
-      var nextExpected = expected.shift();
-
-      child.stdout.on('data', function(chunk) {
-        output += chunk.toString();
-
-        if (nextExpected && output.indexOf(nextExpected.q) != -1) {
-          if (typeof nextExpected.r === 'function') {
-            child.stdin.write(nextExpected.r() + '\n');
-          } else {
-            child.stdin.write(nextExpected.r + '\n');
-          }
-
-          if (expected.length > 0) {
-            nextExpected = expected.shift();
-            output = '';
-          } else {
-            nextExpected = null;
-          }
-        }
-      });
-    }
-
-    child.on('exit', function(code, signal) {
-      if (code === 0) {
-        resolve(code);
-      } else {
-        reject(code);
-      }
-    });
-
-    child.on('error', function(err) {
-      reject(err);
-    });
-  });
-}
-
-function getTokenId(page) {
-  page = page || 1;
-
-  return new Promise(function(resolve, reject) {
-    github.authorization.getAll({
-      page: page,
-      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
-    }, function(err, res) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      for (var i = 0; i < res.length; i++) {
-        if (res[i].note === 'test' && res[i].note_url === 'http://www.test.org') {
-          resolve(res[i].id);
-          return;
-        }
-      }
-
-      resolve(getToken(++page));
-    });
-  });
-}
-
-function deleteAuthorization(tokenId) {
-  return new Promise(function(resolve, reject) {
-    github.authorization.delete({
-      id: tokenId,
-      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
-    }, function(err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function createAuthorization() {
-  return new Promise(function(resolve, reject) {
-    github.authorization.create({
-      scopes: ['repo', 'public_repo', 'delete_repo'],
-      note: 'test',
-      note_url: 'http://www.test.org',
-      headers: useOTP ? { 'X-GitHub-OTP': readlineSync.question('Auth Code: ') } : {},
-    }, function(err, res) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      githubToken = res.token;
-      githubTokenId = res.id;
-
-      github.authenticate({
-        type: 'oauth',
-        token: githubToken,
-      });
-
-      resolve(res);
-    });
-  }).catch(function(err) {
-    var error = JSON.parse(err.message);
-
-    if (error.message === 'Must specify two-factor authentication OTP code.') {
-      useOTP = true;
-      return createAuthorization();
-    }
-
-    if (error.message === 'Validation Failed' && error.errors[0].code === 'already_exists') {
-      return getTokenId().then(deleteAuthorization).then(createAuthorization);
-    }
-
-    throw err;
-  });
 }
 
 describe('CLI interface, oghliner as a tool', function() {
@@ -208,21 +20,15 @@ describe('CLI interface, oghliner as a tool', function() {
   var oldWD = process.cwd();
 
   before(function() {
-    github.authenticate({
-      type: 'basic',
-      username: username,
-      password: password,
-    });
-
-    return createAuthorization();
+    return liveUtils.createAuthorization(username, password);
   });
 
   beforeEach(function() {
     process.chdir(temp.mkdirSync('oghliner'));
 
-    process.env.GH_TOKEN = username + ':' + githubToken;
+    process.env.GH_TOKEN = username + ':' + liveUtils.githubToken;
 
-    return deleteRepo()
+    return liveUtils.deleteRepo(username)
     .catch(function() {
       // Ignore error if the repo doesn't exist.
     });
@@ -235,23 +41,23 @@ describe('CLI interface, oghliner as a tool', function() {
   });
 
   it('should work', function() {
-    return createRepo()
-    .then(spawn.bind(null, 'git', ['clone', 'https://' + username + ':' + githubToken + '@github.com/' + username + '/test_oghliner_live']))
+    return liveUtils.createRepo()
+    .then(liveUtils.spawn.bind(null, 'git', ['clone', 'https://' + username + ':' + liveUtils.githubToken + '@github.com/' + username + '/test_oghliner_live']))
     .then(process.chdir.bind(null, 'test_oghliner_live'))
-    .then(spawn.bind(null, 'npm', ['install', path.dirname(__dirname)]))
+    .then(liveUtils.spawn.bind(null, 'npm', ['install', path.dirname(__dirname)]))
     .then(function() {
       fse.mkdirSync('dist');
     })
     .then(fse.writeFileSync.bind(fse, 'dist/index.html', '<html></html>'))
-    .then(spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['offline', 'dist']))
-    .then(spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['integrate', 'dist']))
-    .then(spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['deploy', 'dist']))
+    .then(liveUtils.spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['offline', 'dist']))
+    .then(liveUtils.spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['integrate', 'dist']))
+    .then(liveUtils.spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['deploy', 'dist']))
     .then(function() {
-      return getBranch()
-      .catch(getBranch)
-      .catch(getBranch)
+      return liveUtils.getBranch(username)
+      .catch(liveUtils.getBranch.bind(null, username))
+      .catch(liveUtils.getBranch.bind(null, username))
     })
-    .then(spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['configure'], [
+    .then(liveUtils.spawn.bind(null, path.join('node_modules', '.bin', 'oghliner'), ['configure'], [
       {
         q: 'Username: ',
         r: username,
@@ -290,8 +96,8 @@ describe('CLI interface, oghliner as a tool', function() {
         fse.removeSync(file);
       });
     })
-    .then(spawn.bind(null, 'git', ['checkout', '-b', 'gh-pages']))
-    .then(spawn.bind(null, 'git', ['pull', 'origin', 'gh-pages']))
+    .then(liveUtils.spawn.bind(null, 'git', ['checkout', '-b', 'gh-pages']))
+    .then(liveUtils.spawn.bind(null, 'git', ['pull', 'origin', 'gh-pages']))
     .then(function() {
       assert.doesNotThrow(fse.statSync.bind(fse, 'offline-manager.js'));
       assert.doesNotThrow(fse.statSync.bind(fse, 'offline-worker.js'));
