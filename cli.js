3a5409e4327f368bcd210747baf7fefe047938b8
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-11-13 14:06:55 +0000

    Don't require unused rimraf

--- cli.js
+++ cli.js
@@ -23,7 +23,6 @@ var promisify = require("promisify-node");
 
 var packageJson = require('./package.json');
 var program = require('commander');
-var rimraf = promisify(require('rimraf'));
 var promptly = require('promisified-promptly');
 var fs = require('fs');
 var chalk = require('chalk');
@@ -64,7 +63,7 @@ program
     .then(function() {
       // For perf, don't delete the repository.  This means users will have to
       // add .gh-pages-cache to their .gitignore file to hide its `git status`.
-      // return rimraf('.gh-pages-cache');
+      // return fse.removeSync('.gh-pages-cache');
 
       fs.access('.gitignore', function(err) {
         if (err) {
