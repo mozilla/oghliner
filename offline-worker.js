5247d44e9aeaa6845e66a5b867e7c84fff51ac6e
Travis-CI <myk@mykzilla.org>
2015-11-21 18:35:13 +0000

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -1,220 +1,145 @@
-/**
- * Copyright 2015 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// This generated service worker JavaScript will precache your site's resources.
-// The code needs to be saved in a .js file at the top-level of your site, and registered
-// from your pages in order to be used. See
-// https://github.com/googlechrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
-// for an example of how you can register this script and handle various service worker events.
-
-/* eslint-env worker, serviceworker */
-/* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren */
-'use strict';
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 
+(function (self) {
+  'use strict';
 
-/* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["images/apple-touch-icon-114x114.png","5078e46e4b45d84e7e652943cfa0fe5c"],["images/apple-touch-icon-120x120.png","f18a85f267651909f82c69473fe54990"],["images/apple-touch-icon-144x144.png","3c590100538aa518e1f503eecebf44a2"],["images/apple-touch-icon-152x152.png","3266b2d239f63a1051920fe31c1f8748"],["images/apple-touch-icon-57x57.png","043bfdba8ed5636f703d196e5fa6a8f3"],["images/apple-touch-icon-60x60.png","db6a053f2abe26966a8e639384a04a9b"],["images/apple-touch-icon-72x72.png","ca97a5c1750bc3346b0180271b9b1d6a"],["images/apple-touch-icon-76x76.png","63bead3c1c1d78352783742aef0212b6"],["images/favicon-128x128.png","7823690c74ffa3e2e975521cf41a47f0"],["images/favicon-16x16.png","f29fa7df9edb217557bdb8cba7fdc2e4"],["images/favicon-196x196.png","aece9b08bd756f6d5f950753dc59a699"],["images/favicon-32x32.png","c0912fde3533f41bfe7448e26c2a0f09"],["images/favicon-96x96.png","94989695aa6e60e9b8ca145c8d75523a"],["images/mstile-144x144.png","05c9dc5222205975991465ff3a2e66e5"],["images/mstile-150x150.png","4dd7ae7adb25741f7076f57bcebf489f"],["images/mstile-310x150.png","be8fc9dd5509a7d831b9eaee43916405"],["images/mstile-310x310.png","a8f0750e7db7f7087bcac055e340d938"],["images/mstile-70x70.png","df013bdddc95f442295f7940f1400def"],["index.html","abc1623c0d38276efe1f790fe45ea814"],["scripts/offline-manager.js","d2b04cba2f0c3d1f55a430da0aa4c9e1"],["styles/stylesheet.css","61d998ba02b7caf9e5e61ad5dc582561"],["styles/tabzilla/css/tabzilla.css","2f04e7f2594724cdf47767b3bc15295d"],["styles/tabzilla/media/img/tabzilla-static-high-res.png","bac5ff04ca0700df8c66a85635d2dba9"],["styles/tabzilla/media/img/tabzilla-static.png","6675a96dec104d786f9cff43c76fa6f3"]];
-/* eslint-enable quotes, comma-spacing */
-var CacheNamePrefix = 'sw-precache-v1-mozilla/oghliner-' + (self.registration ? self.registration.scope : '') + '-';
-
-
-var IgnoreUrlParametersMatching = [/./];
-
-
-
-var addDirectoryIndex = function (originalUrl, index) {
-    var url = new URL(originalUrl);
-    if (url.pathname.slice(-1) === '/') {
-      url.pathname += index;
-    }
-    return url.toString();
-  };
-
-var populateCurrentCacheNames = function (precacheConfig, cacheNamePrefix, baseUrl) {
-    var absoluteUrlToCacheName = {};
-    var currentCacheNamesToAbsoluteUrl = {};
-
-    precacheConfig.forEach(function(cacheOption) {
-      var absoluteUrl = new URL(cacheOption[0], baseUrl).toString();
-      var cacheName = cacheNamePrefix + absoluteUrl + '-' + cacheOption[1];
-      currentCacheNamesToAbsoluteUrl[cacheName] = absoluteUrl;
-      absoluteUrlToCacheName[absoluteUrl] = cacheName;
-    });
-
-    return {
-      absoluteUrlToCacheName: absoluteUrlToCacheName,
-      currentCacheNamesToAbsoluteUrl: currentCacheNamesToAbsoluteUrl
-    };
-  };
-
-var stripIgnoredUrlParameters = function (originalUrl, ignoreUrlParametersMatching) {
-    var url = new URL(originalUrl);
-
-    url.search = url.search.slice(1) // Exclude initial '?'
-      .split('&') // Split into an array of 'key=value' strings
-      .map(function(kv) {
-        return kv.split('='); // Split each 'key=value' string into a [key, value] array
-      })
-      .filter(function(kv) {
-        return ignoreUrlParametersMatching.every(function(ignoredRegex) {
-          return !ignoredRegex.test(kv[0]); // Return true iff the key doesn't match any of the regexes.
-        });
-      })
-      .map(function(kv) {
-        return kv.join('='); // Join each [key, value] array into a 'key=value' string
-      })
-      .join('&'); // Join the array of 'key=value' strings into a string with '&' in between each
-
-    return url.toString();
-  };
-
-
-var mappings = populateCurrentCacheNames(PrecacheConfig, CacheNamePrefix, self.location);
-var AbsoluteUrlToCacheName = mappings.absoluteUrlToCacheName;
-var CurrentCacheNamesToAbsoluteUrl = mappings.currentCacheNamesToAbsoluteUrl;
-
-function deleteAllCaches() {
-  return caches.keys().then(function(cacheNames) {
-    return Promise.all(
-      cacheNames.map(function(cacheName) {
-        return caches.delete(cacheName);
-      })
-    );
+  // On install, cache resources and skip waiting so the worker won't
+  // wait for clients to be closed before becoming active.
+  self.addEventListener('install', function (event) {
+    event.waitUntil(oghliner.cacheResources().then(function () {
+      return self.skipWaiting();
+    }));
   });
-}
 
-self.addEventListener('install', function(event) {
-  var now = Date.now();
+  // On activation, delete old caches and start controlling the clients
+  // without waiting for them to reload.
+  self.addEventListener('activate', function (event) {
+    event.waitUntil(oghliner.clearOtherCaches().then(function () {
+      return self.clients.claim();
+    }));
+  });
 
-  event.waitUntil(
-    caches.keys().then(function(allCacheNames) {
-      return Promise.all(
-        Object.keys(CurrentCacheNamesToAbsoluteUrl).filter(function(cacheName) {
-          return allCacheNames.indexOf(cacheName) === -1;
-        }).map(function(cacheName) {
-          var url = new URL(CurrentCacheNamesToAbsoluteUrl[cacheName]);
-          // Put in a cache-busting parameter to ensure we're caching a fresh response.
-          if (url.search) {
-            url.search += '&';
-          }
-          url.search += 'sw-precache=' + now;
-          var urlWithCacheBusting = url.toString();
+  // Retrieves the request following oghliner strategy.
+  self.addEventListener('fetch', function (event) {
+    if (event.request.method === 'GET') {
+      event.respondWith(oghliner.get(event.request));
+    } else {
+      event.respondWith(self.fetch(event.request));
+    }
+  });
 
-          console.log('Adding URL "%s" to cache named "%s"', urlWithCacheBusting, cacheName);
-          return caches.open(cacheName).then(function(cache) {
-            var request = new Request(urlWithCacheBusting, {credentials: 'same-origin'});
-            return fetch(request.clone()).then(function(response) {
-              if (response.ok) {
-                return cache.put(request, response);
-              }
+  var oghliner = self.oghliner = {
 
-              console.error('Request for %s returned a response with status %d, so not attempting to cache it.',
-                urlWithCacheBusting, response.status);
-              // Get rid of the empty cache if we can't add a successful response to it.
-              return caches.delete(cacheName);
-            });
+    // This is the unique prefix for all the caches controlled by this worker.
+    CACHE_PREFIX: 'offline-cache:mozilla/oghliner:' + (self.registration ? self.registration.scope : '') + ':',
+
+    // This is the unique name for the cache controlled by this version of the worker.
+    get CACHE_NAME() {
+      return this.CACHE_PREFIX + 'e10407a301c62858f7818663c60b9cf78562c792';
+    },
+
+    // This is a list of resources that will be cached.
+    RESOURCES: [
+      './', // cache always the current root to make the default page available
+      './images/apple-touch-icon-114x114.png', // a59b2a2e1f96569e9bddc6bdb2e75b535f50740c
+      './images/apple-touch-icon-120x120.png', // eb044227a288f2c496c47b4a42f3e1ffba913c84
+      './images/apple-touch-icon-144x144.png', // d192a72ce1124afebacb85fc3c65deabc952f09a
+      './images/apple-touch-icon-152x152.png', // 737408359072454bd487f2610bd0c4a1478d8852
+      './images/apple-touch-icon-57x57.png', // d14e9a69cc78ed3b8eb6597c6d90665a3ecc2feb
+      './images/apple-touch-icon-60x60.png', // d19e5a92de4725a5fa418175db568d52fdd53258
+      './images/apple-touch-icon-72x72.png', // aaf792166ead2a5087802cfd93219dd6e67270e7
+      './images/apple-touch-icon-76x76.png', // 6978926be3c5d8d86c6d620eaad0e4935f0af004
+      './images/favicon-128x128.png', // 3ccb0249a0594a52e7a0bd1bf0322cf853d356b4
+      './images/favicon-16x16.png', // 74a77aae27835823a307df943d0e249bf868b1d9
+      './images/favicon-196x196.png', // a82f166562b5f0a1f651937d4a27e8dec5bb565e
+      './images/favicon-32x32.png', // 3d8a4affe635d9ee98270e7bdbf75b59bdeace51
+      './images/favicon-96x96.png', // ddeb536045b57431fb87452cc308667ebe5a9dcd
+      './images/mstile-144x144.png', // cca0e49c55f944ae02f08d37560f0a0073ec8902
+      './images/mstile-150x150.png', // 64da27a0c359f1c0a86f75aa1aaf65a51bc57dc2
+      './images/mstile-310x150.png', // d100f26874fb944f7f7fefd80929465c27b27db5
+      './images/mstile-310x310.png', // 292e53d8db6bc724211aaa2405afe15dc9a7d559
+      './images/mstile-70x70.png', // b053002ff86b9d407ac7173583627404a78a6cf9
+      './index.html', // c63d0cc6d312b5498029ef86ddb06e97b2e7d2d8
+      './scripts/offline-manager.js', // e2e09e000c5b64035940ae44e9c0936eb25ecd51
+      './styles/stylesheet.css', // 36e6741b394816aa3a8ff34c7b77c251504884ac
+      './styles/tabzilla/css/tabzilla.css', // 470406a43e2498fb499b4ab174906d6d2a3a8cb5
+      './styles/tabzilla/media/img/tabzilla-static-high-res.png', // b61a9911763194807cdd009d9772d8f74d9219f4
+      './styles/tabzilla/media/img/tabzilla-static.png', // daf1c7682b6197942b1c82b0790f57bf9605a13c
+
+    ],
+
+    // Adds the resources to the cache controlled by this worker.
+    cacheResources: function () {
+      var now = Date.now();
+      var baseUrl = self.location;
+      return this.prepareCache()
+      .then(function (cache) {
+        return Promise.all(this.RESOURCES.map(function (resource) {
+          // Bust the request to get a fresh response
+          var url = new URL(resource, baseUrl);
+          var bustParameter = (url.search ? '&' : '') + '__bust=' + now;
+          var bustedUrl = new URL(url.toString());
+          bustedUrl.search += bustParameter;
+
+          // But cache the response for the original request
+          var requestConfig = { credentials: 'same-origin' };
+          var originalRequest = new Request(url.toString(), requestConfig);
+          var bustedRequest = new Request(bustedUrl.toString(), requestConfig);
+          return fetch(bustedRequest).then(function (response) {
+            if (response.ok) {
+              return cache.put(originalRequest, response);
+            }
+            console.error('Error fetching ' + url + ', status was ' + response.status);
           });
-        })
-      ).then(function() {
-        return Promise.all(
-          allCacheNames.filter(function(cacheName) {
-            return cacheName.indexOf(CacheNamePrefix) === 0 &&
-                   !(cacheName in CurrentCacheNamesToAbsoluteUrl);
-          }).map(function(cacheName) {
-            console.log('Deleting out-of-date cache "%s"', cacheName);
-            return caches.delete(cacheName);
-          })
-        );
+        }));
+      }.bind(this));
+    },
+
+    // Remove the offline caches not controlled by this worker.
+    clearOtherCaches: function () {
+      var deleteIfNotCurrent = function (cacheName) {
+        if (cacheName.indexOf(this.CACHE_PREFIX) !== 0 || cacheName === this.CACHE_NAME) {
+          return Promise.resolve();
+        }
+        return self.caches.delete(cacheName);
+      }.bind(self);
+
+      return self.caches.keys()
+      .then(function (cacheNames) {
+        return Promise.all(cacheNames.map(deleteIfNotCurrent));
       });
-    }).then(function() {
-      if (typeof self.skipWaiting === 'function') {
-        // Force the SW to transition from installing -> active state
-        self.skipWaiting();
+
+    },
+
+    // Get a response from the current offline cache or from the network.
+    get: function (request) {
+      return this.openCache()
+      .then(function (cache) {
+        return cache.match(request);
+      })
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return self.fetch(request);
+      });
+    },
+
+    // Prepare the cache for installation, deleting it before if it already exists.
+    prepareCache: function () {
+      return self.caches.delete(this.CACHE_NAME).then(this.openCache.bind(this));
+    },
+
+    // Open and cache the offline cache promise to improve the performance when
+    // serving from the offline-cache.
+    openCache: function () {
+      if (!this._cache) {
+        this._cache = self.caches.open(this.CACHE_NAME);
       }
-    })
-  );
-});
-
-if (self.clients && (typeof self.clients.claim === 'function')) {
-  self.addEventListener('activate', function(event) {
-    event.waitUntil(self.clients.claim());
-  });
-}
-
-self.addEventListener('message', function(event) {
-  if (event.data.command === 'delete_all') {
-    console.log('About to delete all caches...');
-    deleteAllCaches().then(function() {
-      console.log('Caches deleted.');
-      event.ports[0].postMessage({
-        error: null
-      });
-    }).catch(function(error) {
-      console.log('Caches not deleted:', error);
-      event.ports[0].postMessage({
-        error: error
-      });
-    });
-  }
-});
-
-
-self.addEventListener('fetch', function(event) {
-  if (event.request.method === 'GET') {
-    var urlWithoutIgnoredParameters = stripIgnoredUrlParameters(event.request.url,
-      IgnoreUrlParametersMatching);
-
-    var cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
-    var directoryIndex = 'index.html';
-    if (!cacheName && directoryIndex) {
-      urlWithoutIgnoredParameters = addDirectoryIndex(urlWithoutIgnoredParameters, directoryIndex);
-      cacheName = AbsoluteUrlToCacheName[urlWithoutIgnoredParameters];
+      return this._cache;
     }
 
-    var navigateFallback = '';
-    // Ideally, this would check for event.request.mode === 'navigate', but that is not widely
-    // supported yet:
-    // https://code.google.com/p/chromium/issues/detail?id=540967
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1209081
-    if (!cacheName && navigateFallback && event.request.headers.has('accept') &&
-        event.request.headers.get('accept').includes('text/html')) {
-      var navigateFallbackUrl = new URL(navigateFallback, self.location);
-      cacheName = AbsoluteUrlToCacheName[navigateFallbackUrl.toString()];
-    }
-
-    if (cacheName) {
-      event.respondWith(
-        // We can't call cache.match(event.request) since the entry in the cache will contain the
-        // cache-busting parameter. Instead, rely on the fact that each cache should only have one
-        // entry, and return that.
-        caches.open(cacheName).then(function(cache) {
-          return cache.keys().then(function(keys) {
-            return cache.match(keys[0]).then(function(response) {
-              return response || fetch(event.request).catch(function(e) {
-                console.error('Fetch for "%s" failed: %O', urlWithoutIgnoredParameters, e);
-              });
-            });
-          });
-        }).catch(function(e) {
-          console.error('Couldn\'t serve response for "%s" from cache: %O', urlWithoutIgnoredParameters, e);
-          return fetch(event.request);
-        })
-      );
-    }
-  }
-});
-
+  };
+}(self));
